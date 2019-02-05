41d5f5f47a019e9f449079e41f18b80470b787e3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-05 15:18:50 -0800

    1.0.8: use reciprocal

--- src/material/scalars.ts
+++ src/material/scalars.ts
@@ -1,8 +1,20 @@
-import { apply, Count, from, INITIAL, OCTAVE, Scalar, to, zeroAndPositiveIntegers } from '@musical-patterns/utilities'
+import {
+    apply,
+    Count,
+    from,
+    INITIAL,
+    OCTAVE,
+    reciprocal,
+    Scalar,
+    to,
+    zeroAndPositiveIntegers,
+} from '@musical-patterns/utilities'
 
 const buildMoeomScalars: (steps: Count) => Scalar[] =
     (steps: Count): Scalar[] => {
-        const logarithmicStep: Scalar = to.Scalar(from.Base(apply.Power(OCTAVE, to.Power(1 / from.Count(steps)))))
+        const logarithmicStep: Scalar = to.Scalar(from.Base(
+            apply.Power(OCTAVE, to.Power(reciprocal(from.Count(steps)))),
+        ))
 
         return zeroAndPositiveIntegers.slice(from.Index(INITIAL), from.Count(steps))
             .map((integer: number): Scalar =>
