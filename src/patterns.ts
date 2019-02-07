053d3a46d3b3d6ab4c2a0df8b43ae7fe19eef797
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-07 12:20:55 -0800

    mii

--- src/patterns.ts
+++ src/patterns.ts
@@ -14,7 +14,7 @@ const metadata: Metadata = {
     description: post,
     formattedName: 'Moeom',
     mostRecentPublish: process.env.PUBLISH_DATE || '2018-10-31T07:00:00.000Z',
-    musicalIdeaIllustrated: '',
+    musicalIdeaIllustrated: 'when step sizes increase in pitch circular space, eventually harmonic direction flips',
     originalPublish: '2018-10-31T07:00:00.000Z',
 }
 
