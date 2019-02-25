92bb724112cd89b090fbf50b01e05b370fc4d118
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-25 10:13:25 -0800

    1.0.31: use standard scales

--- src/material/scales.ts
+++ src/material/scales.ts
@@ -1,41 +1,13 @@
 import { BuildScalesFunction, Scale } from '@musical-patterns/compiler'
-import {
-    buildStandardScales,
-    generateOctaveRepeatingScalars,
-    StandardSpecProperties,
-} from '@musical-patterns/pattern'
-import { buildEqualDivisionScalars, from, NO_TRANSLATION, Scalar, to, Translation } from '@musical-patterns/utilities'
+import { buildStandardScales, generateOctaveRepeatingScalars } from '@musical-patterns/pattern'
+import { buildEqualDivisionScalars, Scalar } from '@musical-patterns/utilities'
 import { MoeomSpec } from '../spec'
 
 const buildScales: BuildScalesFunction =
     (spec: MoeomSpec): Scale[] => {
-        const { nonScale } = buildStandardScales()
+        const pitchScalars: Scalar[] = generateOctaveRepeatingScalars(buildEqualDivisionScalars(spec.equalDivision))
 
-        const gainScale: Scale = nonScale
-        const durationScalar: Scalar =
-            from.Ms(spec[ StandardSpecProperties.BASE_DURATION ] || to.Scalar(to.Ms(1)))
-        const durationTranslation: Translation =
-            from.Ms(spec[ StandardSpecProperties.DURATION_TRANSLATION ] || to.Ms(NO_TRANSLATION))
-        const durationsScale: Scale = {
-            scalar: durationScalar,
-            scalars: nonScale.scalars,
-            translation: durationTranslation,
-        }
-        const pitchesScalar: Scalar =
-            from.Hz(spec[ StandardSpecProperties.BASE_FREQUENCY ] || to.Scalar(to.Hz(1)))
-        const pitchesTranslation: Translation =
-            from.Hz(spec[ StandardSpecProperties.FREQUENCY_TRANSLATION ] || to.Hz(NO_TRANSLATION))
-        const pitchesScale: Scale = {
-            scalar: pitchesScalar,
-            scalars: generateOctaveRepeatingScalars(buildEqualDivisionScalars(spec.equalDivision)),
-            translation: pitchesTranslation,
-        }
-
-        return [
-            gainScale,
-            durationsScale,
-            pitchesScale,
-        ]
+        return buildStandardScales(spec, { pitchScalars })
     }
 
 export {
