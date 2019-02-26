5f57f126237efb0b7c5ab8971b6a17b671a1518f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-26 09:56:53 -0800

    1.0.32: semi-units

--- src/material/notes.ts
+++ src/material/notes.ts
@@ -1,6 +1,6 @@
 import { NoteSpec } from '@musical-patterns/compiler'
 import { PitchOnly, STANDARD_DURATIONS_SCALE_INDEX, STANDARD_PITCH_SCALE_INDEX } from '@musical-patterns/pattern'
-import { ContourElement, to } from '@musical-patterns/utilities'
+import { ContourElement, from, Scalar, to } from '@musical-patterns/utilities'
 import { REDUCE_GAIN_BECAUSE_SAMPLES_ARE_SUPER_LOUD } from './constants'
 
 const buildNoteSpec: (pitchOnlyContourElement: ContourElement<PitchOnly>) => NoteSpec =
@@ -9,7 +9,7 @@ const buildNoteSpec: (pitchOnlyContourElement: ContourElement<PitchOnly>) => Not
             scaleIndex: STANDARD_DURATIONS_SCALE_INDEX,
         },
         gainSpec: {
-            scalar: REDUCE_GAIN_BECAUSE_SAMPLES_ARE_SUPER_LOUD,
+            scalar: from.Amplitude(REDUCE_GAIN_BECAUSE_SAMPLES_ARE_SUPER_LOUD) as Scalar,
         },
         pitchSpec: {
             index: to.Ordinal(pitchOnlyContourElement[ 0 ]),
--- src/material/scales.ts
+++ src/material/scales.ts
@@ -1,11 +1,15 @@
 import { BuildScalesFunction, Scale } from '@musical-patterns/compiler'
 import { buildStandardScales, generateOctaveRepeatingScalars } from '@musical-patterns/pattern'
-import { buildEqualDivisionScalars, Scalar } from '@musical-patterns/utilities'
+import { buildEqualDivisionScalars, from, Scalar, to } from '@musical-patterns/utilities'
 import { MoeomSpec } from '../spec'
 
 const buildScales: BuildScalesFunction =
     (spec: MoeomSpec): Scale[] => {
-        const pitchScalars: Scalar[] = generateOctaveRepeatingScalars(buildEqualDivisionScalars(spec.equalDivision))
+        const pitchScalars: Scalar[] =
+            generateOctaveRepeatingScalars(buildEqualDivisionScalars(spec.equalDivision)
+                .map(to.Frequency))
+                .map(from.Frequency)
+                .map(to.Scalar)
 
         return buildStandardScales(spec, { pitchScalars })
     }
