18ab266172d5631fca8da810c12e82b13481fe59
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-09 13:11:26 -0700

    1.0.70: constant case integers

--- src/material/wholes.ts
+++ src/material/wholes.ts
@@ -7,14 +7,14 @@ import {
     sequence,
     slice,
     to,
-    zeroAndPositiveIntegers,
+    ZERO_AND_POSITIVE_INTEGERS,
 } from '@musical-patterns/utilities'
 import { MoeomSpecs } from '../spec'
 import { moeomPiece } from './pieces'
 
 const moeomWhole: (specs: MoeomSpecs) => ContourWhole<PitchOnly> =
     ({ equalDivision, flipped }: MoeomSpecs): ContourWhole<PitchOnly> =>
-        slice(zeroAndPositiveIntegers, INITIAL, to.Ordinal(from.Denominator(equalDivision)))
+        slice(ZERO_AND_POSITIVE_INTEGERS, INITIAL, to.Ordinal(from.Denominator(equalDivision)))
             .map(to.Numerator)
             .reduce(
                 (accumulator: ContourWhole<PitchOnly>, equalDivisionStep: Numerator): ContourWhole<PitchOnly> =>
