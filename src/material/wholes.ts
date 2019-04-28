f63f7c4bcd5e974f221365e21af150fe42845f8a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-27 22:10:13 -0700

    1.0.85: range

--- src/material/wholes.ts
+++ src/material/wholes.ts
@@ -1,20 +1,11 @@
 import { PitchOnly } from '@musical-patterns/material'
-import {
-    as,
-    ContourWhole,
-    INITIAL,
-
-    Numerator,
-    sequence,
-    slice,
-    ZERO_AND_POSITIVE_INTEGERS,
-} from '@musical-patterns/utilities'
+import { as, ContourWhole, Numerator, range, sequence } from '@musical-patterns/utilities'
 import { MoeomSpecs } from '../spec'
 import { moeomPiece } from './pieces'
 
 const moeomWhole: (specs: MoeomSpecs) => ContourWhole<PitchOnly> =
     ({ equalDivision, flipped }: MoeomSpecs): ContourWhole<PitchOnly> =>
-        slice(ZERO_AND_POSITIVE_INTEGERS, INITIAL, as.Ordinal(as.number(equalDivision)))
+        range(equalDivision)
             .map(as.Numerator)
             .reduce(
                 (accumulator: ContourWhole<PitchOnly>, equalDivisionStep: Numerator): ContourWhole<PitchOnly> =>
