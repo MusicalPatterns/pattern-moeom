76ff237a1768c1b5c56331c6741abb756ae6999c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-29 14:46:16 -0700

    1.0.86: musicalAs

--- src/spec/constants.ts
+++ src/spec/constants.ts
@@ -1,11 +1,11 @@
 // tslint:disable no-magic-numbers
 
-import { as, Delta, Denominator, Duration, Hz, Ms, Pitch } from '@musical-patterns/utilities'
+import { as, Denominator, Duration, musicalAs, Pitch } from '@musical-patterns/utilities'
 
 const MOEOM_INITIAL_EQUAL_DIVISION: Denominator = as.Denominator(24)
 const MOEOM_INITIAL_FLIPPED: boolean = false
-const MOEOM_INITIAL_BASIS_DURATION: Duration = as.Delta<Ms>(240)
-const MOEOM_INITIAL_BASIS_FREQUENCY: Pitch = as.Point<Hz>(240)
+const MOEOM_INITIAL_BASIS_DURATION: Duration = musicalAs.Duration(240)
+const MOEOM_INITIAL_BASIS_FREQUENCY: Pitch = musicalAs.Pitch(240)
 
 export {
     MOEOM_INITIAL_EQUAL_DIVISION,
