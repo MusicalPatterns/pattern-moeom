80140b72dbdd35f3d14c3037816d8e7fb286ded7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-02-23 23:42:34 -0800

    1.0.95: thunks, required spces, return types, code formatting, and a big change to xenharmonic series

--- src/material/features.ts
+++ src/material/features.ts
@@ -1,8 +1,4 @@
-import {
-    Note,
-    PitchOnly,
-    REDUCE_GAIN_BECAUSE_SAMPLES_ARE_SUPER_LOUD,
-} from '@musical-patterns/material'
+import { Note, PitchOnly, REDUCE_GAIN_BECAUSE_SAMPLES_ARE_SUPER_LOUD } from '@musical-patterns/material'
 import { as, ContourElement, Pitch, Scalar } from '@musical-patterns/utilities'
 
 const computeNote: (contourElement: ContourElement<PitchOnly>) => Note =
